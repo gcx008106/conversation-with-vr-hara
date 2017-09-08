9442409fe13e6e71f9b21485eec7333264f029e5
Takashi <hara@jfe-systems.com>
2017-09-08 15:53:13 +0900

    Environment variables are moved to .env.

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ var os = require('os');
 // from conversation
 var bodyParser = require('body-parser'); // parser for post requests
 var Conversation = require('watson-developer-cloud/conversation/v1'); // watson sdk
+const queryBuilder = require('./query-builder');
 
 var ONE_HOUR = 3600000;
 var TWENTY_SECONDS = 20000;
@@ -36,48 +37,28 @@ var TWENTY_SECONDS = 20000;
 // Bootstrap application settings
 require('./config/express')(app);
 
-// Create the service wrapper
-// If no API Key is provided here, the watson-developer-cloud@2.x.x library will check for an VISUAL_RECOGNITION_API_KEY
-// environment property and then fall back to the VCAP_SERVICES property provided by Bluemix.
-var visualRecognition = new watson.VisualRecognitionV3({
-  api_key: '81fc32afd6d4099e843756062a4c9afec035a73a',
-  version_date: '2015-05-19'
-});
-
-
-// Create the service wrapper
+// Conversation
 var conversation = new Conversation({
-  // If unspecified here, the CONVERSATION_USERNAME and CONVERSATION_PASSWORD env properties will be checked
-  // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
-  username: '96ed59fb-76c5-41cf-a293-c4424f16db7f',
-  password: 'WEUqOb67HnwB',
+  username: process.env.OONVERSATION_USERNAME,
+  password: process.env.CONVERSATION_PASSWORD,  
   url: 'https://gateway.watsonplatform.net/conversation/api',
-  //WORKSPACE_ID=cc5d1593-8802-4126-9793-c47da9b6c39d
   version_date: Conversation.VERSION_DATE_2017_04_21
 });
 
-
 /// Discovery 
-const queryBuilder = require('./query-builder');
-
-//const NEWS_ENVIRONMENT_ID = 'system';
-//const NEWS_COLLECTION_ID = 'news';
-
-//Discovery-Demo-hara:SmaProto
-const ENVIRONMENT_ID = 'b9c6ac08-1e20-4129-83d9-9c4e14116fb2';
-const COLLECTION_ID = 'a0c4668a-954b-4a44-a480-b89ed93b6a70';
-
 const DiscoveryV1 = require('watson-developer-cloud/discovery/v1');
 const discovery = new DiscoveryV1({
-  // If unspecified here, the DISCOVERY_USERNAME and
-  // DISCOVERY_PASSWORD env properties will be checked
-  // After that, the SDK will fall back to the bluemix-provided VCAP_SERVICES environment property
-  username: '630e6f15-ab51-46cb-be96-86b1676be38b',
-  password: 'zRaiH4aoOORw',
-
+  username: process.env.DISCOVERY_USERNAME,
+  password: process.env.DISCOVERY_PASSWORD,
   version_date: '2017-08-01',
   qs: { aggregation: `[${queryBuilder.aggregations.join(',')}]` },
 }); 
+
+// Visual Recognition
+var visualRecognition = new watson.VisualRecognitionV3({
+  api_key: process.env.VISUAL_RECOGNITION_API_KEY,
+  version_date: '2015-05-19'
+});
 
 
 
@@ -87,10 +68,13 @@ app.get('/', function(req, res) {
   });
 });
 
-// Endpoint to be call from the client side
+
+/////////////////////////////////////////////////////////////////////
+// Conversation
+/////////////////////////////////////////////////////////////////////
 app.post('/api/message', function(req, res) {
-  var workspace = process.env.WORKSPACE_ID || '<workspace-id>';
-  if (!workspace || workspace === '<workspace-id>') {
+  var workspace = process.env.CONVERSATION_WORKSPACE_ID;
+  if (!workspace) {
     return res.json({
       'output': {
         'text': 'The app has not been configured with a <b>WORKSPACE_ID</b> environment variable. Please refer to the ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple">README</a> documentation on how to set this variable. <br>' + 'Once a workspace has been defined the intents may be imported from ' + '<a href="https://github.com/watson-developer-cloud/conversation-simple/blob/master/training/car_workspace.json">here</a> in order to get a working application.'
@@ -111,6 +95,7 @@ app.post('/api/message', function(req, res) {
     return res.json(updateMessage(payload, data));
   });
 });
+
 
 /**
  * Updates the response text using the intent confidence
@@ -145,6 +130,9 @@ function updateMessage(input, response) {
 }
 
 
+/////////////////////////////////////////////////////////////////////
+// Visual Recognition
+/////////////////////////////////////////////////////////////////////
 
 var scoreData = function(score) {
   var scoreColor;
@@ -206,6 +194,8 @@ function parseBase64Image(imageString) {
  *                     images/test.jpg or https://example.com/test.jpg
  * @param req.file The image file.
  */
+ 
+ 
 app.post('/api/classify', app.upload.single('images_file'), function(req, res) {
   var params = {
     url: null,
@@ -302,6 +292,12 @@ app.post('/api/classify', app.upload.single('images_file'), function(req, res) {
   });
 });
 
+
+
+/////////////////////////////////////////////////////////////////////
+// Discovery
+/////////////////////////////////////////////////////////////////////
+
 // setup query endpoint for news
 app.post('/api/query', (req, res, next) => {
   //console.log("/api/query1");
@@ -311,11 +307,11 @@ app.post('/api/query', (req, res, next) => {
   });*/
 
   const params = Object.assign({}, queryBuilder.build(req.body), {
-    environment_id: ENVIRONMENT_ID,
-    collection_id: COLLECTION_ID
+    environment_id: process.env.DISCOVERY_ENVIRONMENT_ID,
+    collection_id: process.env.DISCOVERY_COLLECTION_ID
   });
   
-  console.log(JSON.stringify(params));
+  //console.log(JSON.stringify(params));
   
   //console.log("/api/query2");
   discovery.query(params, (error, response) => {
@@ -324,7 +320,7 @@ app.post('/api/query', (req, res, next) => {
 	  //console.log("/api/query4 error");  
       next(error);
     } else {
-	  console.log("/api/query5 response="+JSON.stringify(response));  
+	  //console.log("/api/query5 response="+JSON.stringify(response));  
       res.json(response);
     }
   });
